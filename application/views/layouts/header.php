b56c0886c35692150c565350ec5502ff26604769
mhaitsam18 <haitsam03@gmail.com>
2023-10-12 01:33:46 +0700

    initial commit

---
+++ application/views/layouts/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		
+	</title>
+</head>
+<body>
+
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/ico" href="<?= base_url('assets/images/logo/janiklampung.png') ?>" />
+	<link rel="icon" type="image/ico" href="<?= base_url('assets/images/logo/janiklampung.png') ?>" />
+	<title><?= $title ?></title>
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/vendors/iconly/bold.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/vendors/perfect-scrollbar/perfect-scrollbar.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/css/all.min.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/vendors/bootstrap-icons2/bootstrap-icons.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/css/app.css') ?>">
+	<!-- <link rel="shortcut icon" href="<?= base_url('assets/images/favicon.svg') ?>" type="image/x-icon"> -->
+
+</head>
+<body>
+	<div id="app">
+		
+		<div id="main">
+			
+			
